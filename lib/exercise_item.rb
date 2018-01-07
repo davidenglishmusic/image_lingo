371140fda4d1bc1ffef984b51060cf0c77da03ee
davidenglishmusic <david@operaonfilm.com>
2018-01-07 16:01:13 -0400

    allows exercise item objects to be instantiated without a source argument

--- lib/exercise_item.rb
+++ lib/exercise_item.rb
@@ -1,7 +1,7 @@
 class ExerciseItem
   attr_accessor :word, :search_term, :answer, :source
 
-  def initialize(word, search_term, answer, source)
+  def initialize(word, search_term, answer, source = nil)
     @word = word
     @search_term = search_term
     @answer = answer
