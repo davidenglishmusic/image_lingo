7a759e6eda976e93dffdd319a7dca781c445eac8
davidenglishmusic <david@operaonfilm.com>
2018-01-07 16:18:04 -0400

    adds quotation marks around the source for the image blocks

--- lib/image_block_generator.rb
+++ lib/image_block_generator.rb
@@ -1,5 +1,5 @@
 class ImageBlockGenerator
   def self.generate(exersize_item)
-    "<div><img src=#{exersize_item.source}><input type=\"text\" data-answer=\"#{exersize_item.answer}\"></div>"
+    "<div><img src=\"#{exersize_item.source}\"><input type=\"text\" data-answer=\"#{exersize_item.answer}\"></div>"
   end
 end
