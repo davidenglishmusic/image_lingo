8259f783ee07279212739d9dd783cb303fa5f305
davidenglishmusic <david@operaonfilm.com>
2018-01-07 17:19:39 -0400

    implements page with exercises generation

--- lib/document_assembler.rb
+++ lib/document_assembler.rb
@@ -1,17 +1,20 @@
 class DocumentAssembler
-  def self.assemble
-    basic_document
+  def self.assemble(exercises)
+    add_exercises(basic_document, exercises)
   end
 
   def self.basic_document
     template_html = File.open('assets/html/template.html', 'r').read
     template_css = File.open('assets/css/template.css', 'r').read
     template_js = File.open('assets/js/template.js', 'r').read
-    jquery_js = File.open('assets/js/jquery-3.2.1.min.js', 'r').read
 
     template_html.gsub!('<%= css %>', template_css)
     template_html.gsub!('<%= js %>', template_js)
-    template_html.gsub!('<%= jquery %>', jquery_js)
     template_html
+  end
+
+  def self.add_exercises(document, exercises)
+    exercise_markup = exercises.map { |exercise| ImageBlockGenerator.generate(exercise) }.join("\n")
+    document.gsub!('<%= exercises %>', exercise_markup)
   end
 end
