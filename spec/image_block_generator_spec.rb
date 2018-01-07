8f7d399365e96784dd465727d9e3af6e488ddd1e
davidenglishmusic <david@operaonfilm.com>
2018-01-07 17:13:44 -0400

    respecs the image block generator to expect classes and a div with a background image

--- spec/image_block_generator_spec.rb
+++ spec/image_block_generator_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe ImageBlockGenerator do
     it 'returns the markup for an exercise item' do
       exercise_item = ExerciseItem.new('apple', 'fruit apple', 'jablko', 'https://www.fruit.com/apple.jpg')
       expect(ImageBlockGenerator.generate(exercise_item))
-        .to eql('<div><img src="https://www.fruit.com/apple.jpg"><input type="text" data-answer="jablko"></div>')
+        .to eql('<div class="exercise-item"><div class="photo" style="background-image:url(\'https://www.fruit.com/apple.jpg\')"></div><input type="text" data-answer="jablko"></div>')
     end
   end
 end
