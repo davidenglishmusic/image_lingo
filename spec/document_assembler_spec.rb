b16f62ad9888028b046dbca38fb711255c7ab7fe
davidenglishmusic <david@operaonfilm.com>
2018-01-07 17:18:37 -0400

    specs page with exercises generation

--- spec/document_assembler_spec.rb
+++ spec/document_assembler_spec.rb
@@ -1,12 +1,22 @@
 require 'document_assembler'
+require 'exercise_item_generator'
 
 RSpec.describe DocumentAssembler do
   describe '#basic_document' do
-    it 'returns the basic document with inlined javascript and css' do
+    xit 'returns the basic document with inlined javascript and css' do
       basic_document = DocumentAssembler.basic_document
       expect(basic_document).to include('<!-- Image Lingo HTML -->')
       expect(basic_document).to include('/* Image Lingo CSS */')
       expect(basic_document).to include('/* Image Lingo JS */')
+    end
+  end
+
+  describe '#assemble' do
+    it 'returns a document with exercises' do
+      exercises = ExerciseItemGenerator.items_from_json_file('spec/fixtures/czech.json')
+      exercises.each { |exercise| exercise.source = 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Red_Apple.jpg/265px-Red_Apple.jpg' }
+      expect(DocumentAssembler.assemble(exercises))
+        .to include '<div class="exercise-item"><div class="photo" style="background-image:url(\'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Red_Apple.jpg/265px-Red_Apple.jpg\')"></div><input type="text" data-answer="jablko"></div>'
     end
   end
 end
