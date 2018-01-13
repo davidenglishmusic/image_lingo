0c8aafee89bc4f84c025cd5076efca5fdf8758ee
davidenglishmusic <david@operaonfilm.com>
2018-01-13 10:47:32 -0400

    disables and highlights text inputs once the correct answer has been entered

--- assets/js/template.js
+++ assets/js/template.js
@@ -1,4 +1,10 @@
 /* Image Lingo JS */
-$( document ).ready(function() {
-    $('.exercise-item input').val("");
+$(document).ready(function() {
+  $('.exercise-item input').val("");
+  $('.exercise-item input').keyup(function() {
+    if ($(this).val() === $(this).data('answer')) {
+      $(this).prop('disabled', true);
+      $(this).addClass('correct');
+    }
+  });
 });
