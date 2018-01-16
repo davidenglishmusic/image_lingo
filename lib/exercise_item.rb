ae058373daf38fcb182c875e964c42fdfcc9be0f
davidenglishmusic <david@operaonfilm.com>
2018-01-15 20:47:20 -0400

    handles a keyboard friendly answer parameter

--- lib/exercise_item.rb
+++ lib/exercise_item.rb
@@ -1,10 +1,11 @@
 class ExerciseItem
-  attr_accessor :word, :search_terms, :answer, :source
+  attr_accessor :word, :search_terms, :answer, :keyboard_answer, :source
 
-  def initialize(word, search_terms, answer, source = nil)
+  def initialize(word, search_terms, answer, keyboard_answer = nil, source = nil)
     @word = word
     @search_terms = search_terms
     @answer = answer
+    @keyboard_answer = keyboard_answer ? keyboard_answer : answer
     @source = source
   end
 end
--- lib/image_block_generator.rb
+++ lib/image_block_generator.rb
@@ -1,5 +1,10 @@
 class ImageBlockGenerator
   def self.generate(exersize_item)
-    "<div class=\"exercise-item\"><div class=\"photo\" style=\"background-image:url('#{exersize_item.source}')\"></div><input type=\"text\" data-answer=\"#{exersize_item.answer}\"></div>"
+    '<div class="exercise-item"><div class="photo" ' \
+    "style=\"background-image:url('#{exersize_item.source}')\"></div>" \
+    '<input type="text" ' \
+    "data-answer=\"#{exersize_item.answer}\" " \
+    "data-keyboard-answer=\"#{exersize_item.keyboard_answer}\"" \
+    '></div>'
   end
 end
